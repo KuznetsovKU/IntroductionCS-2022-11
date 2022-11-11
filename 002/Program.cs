34f61f4a6f6e606714f1543f132f489304c19b32
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-11 21:59:40 +0300

    done second exercise

---
+++ 002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вписать условие задачи
+System.Console.WriteLine("Введите число num1:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число num2:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+if (num1*num1 == num2) 
+{
+    System.Console.WriteLine($"{num2} является квадратом {num1}");
+}
+if (num2*num2 == num1)
+{
+    System.Console.WriteLine($"{num1} является квадратом {num2}");
+}
