74017c819b2e6106b5df935050a6ff915d47fe40
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-11 21:50:09 +0300

    done first exercise

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Hide this  
+// Console.WriteLine("Hello, World!"); 
+#endregion
+
+// Ввести условие задачи
+int a;
+Console.Write("Введите число ");
+string num = Console.ReadLine();
+a=Convert.ToInt32(num);
+int b=a*a;
+System.Console.WriteLine(a+"^2="+b);
+System.Console.WriteLine($"{a}^2={b}");
+
+
