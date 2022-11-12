61ee527151473d767380a258a11794065bf102a0
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 18:59:33 +0300

    done exercise 9

---
+++ 009/Program.cs
@@ -0,0 +1,12 @@
+﻿// 9.	Вывести на экран четные числа от 1 до N
+
+System.Console.Write("Введите число num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= num)
+{
+    System.Console.Write(i);
+    i++;
+}
