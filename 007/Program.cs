e1643cf5352151823ed318b7e9821ae860f4ca3c
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 18:29:37 +0300

    done exercise 7

---
+++ 007/Program.cs
@@ -0,0 +1,13 @@
+﻿// 7.	Выяснить является ли число чётным.
+
+System.Console.Write("Введите число num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    System.Console.WriteLine("Введенное число является четным");
+}
+else
+{
+    System.Console.WriteLine("Введенное число НЕ является четным");
+}
