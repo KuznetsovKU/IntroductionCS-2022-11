ad2316a4e3d8a2d2dd7ce05e1ff71dadb9cdc9e7
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 19:48:12 +0300

    done exercise 12

---
+++ 012/Program.cs
@@ -0,0 +1,13 @@
+﻿// 12.	С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+
+System.Console.Write("Введите число от 10 до 99: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num / 10 > num % 10)
+{
+    System.Console.WriteLine($"Наибольшая цифра введенного числа: {num / 10}");
+}
+else
+{
+    System.Console.WriteLine($"Наибольшая цифра введенного числа: {num % 10}");
+}
