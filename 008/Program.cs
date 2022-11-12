a948cc19a96c5b3d5550c480c0f0550642410ef4
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 18:55:25 +0300

    done exercise 8

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿// 8.	Вывести на экран числа от -N до N
+
+System.Console.Write("Введите число num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = -num;
+
+while (i <= num)
+{
+    System.Console.Write(i);
+    i++;
+}
