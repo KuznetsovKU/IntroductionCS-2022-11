d0ccd85d0eac41e7809ccc2cde65162d4481ecbd
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 19:23:57 +0300

    done exercise 11

---
+++ 011/Program.cs
@@ -0,0 +1,8 @@
+﻿// 11.	Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+System.Console.Write("Введите число больше 9: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sec_last_dig = (num / 10 ) % 10;
+
+System.Console.WriteLine($"В числе {num} вторая цифра с конца: {sec_last_dig}");
