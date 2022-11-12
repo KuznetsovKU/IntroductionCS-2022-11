7925fe101b116f3180136d3efd0219e52e9ddafa
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 19:03:55 +0300

    done exercise 10

---
+++ 010/Program.cs
@@ -0,0 +1,6 @@
+﻿// 10.	Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+System.Console.Write("Введите ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Последняя цифра: {num % 10}");
