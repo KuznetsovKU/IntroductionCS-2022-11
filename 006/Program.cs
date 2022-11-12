5c59a80a59d687c81c78c1b0ade33f5d8c647b00
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-12 18:24:47 +0300

    done exercise 6

---
+++ 006/Program.cs
@@ -0,0 +1,6 @@
+﻿// 6.	Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+
+System.Console.Write("Введите значение угла: ");
+double angle = Convert.ToDouble(Console.ReadLine());
+double sine_entered_angle = Math.Sin(angle);
+System.Console.WriteLine($"Синус угла {angle} равен {sine_entered_angle}");
