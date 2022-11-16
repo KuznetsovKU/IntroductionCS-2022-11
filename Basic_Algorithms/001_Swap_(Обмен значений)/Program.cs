ed6308081c9cef1e86c70b9cda62027ebdcb1096
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-16 23:53:48 +0300

    add Basic Algorithms 001 - 003

---
+++ Basic_Algorithms/001_Swap_(Обмен значений)/Program.cs
@@ -0,0 +1,37 @@
+﻿// Обмен значений двух переменных
+
+int num1 = 5;
+int num2 = 7;
+System.Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+
+/*
+#region Classic case (with third variable)
+
+int numTemp;
+numTemp = num1;
+num1 = num2;
+num2 = numTemp;
+System.Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+    
+#endregion
+*/
+
+/*
+#region Second case (with summary)
+
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+System.Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+    
+#endregion
+*/
+
+#region Third case (learn independetly)
+
+num1 = num1 ^ num2;
+num2 = num1 ^ num2;
+num1 = num1 ^ num2;
+System.Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+
+#endregion
