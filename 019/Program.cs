3d68ff7d00b0e1d5990bd5ba511173c5e5d1c9f3
KuznetsovKU <kuznetsov_k@outlook.com>
2022-11-18 00:36:58 +0300

    019 через цикл

--- 019/Program.cs
+++ 019/Program.cs
@@ -8,17 +8,17 @@ System.Console.WriteLine($"0 1 {Convert.ToInt32(!(false || true)), 6} {Convert.T
 System.Console.WriteLine($"1 0 {Convert.ToInt32(!(true || false)), 6} {Convert.ToInt32(!true && !false), 8}");
 System.Console.WriteLine($"1 1 {Convert.ToInt32(!(true || true)), 6} {Convert.ToInt32(!true && !true), 8}");
 
-/*
-#region Пока что-то не получается
+
+#region Получилось
 
 int x, y;
 bool flag_X, flag_Y;
 
-for (x = 0; x = 2; x++)
+for (x = 0; x <= 2; x++)
 {
     if (x == 0) flag_X = false; else flag_X = true;
     if (x > 1) break;
-    for (y = 0; y = 2; y++)
+    for (y = 0; y <= 2; y++)
     {
         if (y == 0) flag_Y = false; else flag_Y = true;
         if (y > 1) break;
@@ -31,4 +31,4 @@ for (x = 0; x = 2; x++)
 }
 
 #endregion
-*/
+
